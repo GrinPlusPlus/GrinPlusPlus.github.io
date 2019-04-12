ecfc54adae94fa7bce86b1bd2bc156581122bbb1
David Burkett <davidburkett38@gmail.com>
2019-04-12 09:00:27 -0400

    Updates

---
+++ precache-manifest.eb4ba6474953c3d7c80337519ca63c97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "47316d3e830d9fb340bd",
+    "url": "/static/css/main.8ead3a3b.chunk.css"
+  },
+  {
+    "revision": "47316d3e830d9fb340bd",
+    "url": "/static/js/main.aa4a563d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5e2a07f98144661ac1cb",
+    "url": "/static/js/2.74b35634.chunk.js"
+  },
+  {
+    "revision": "62b3efaa0014888c67019d3975c3883a",
+    "url": "/static/media/GrinBanner_WhiteLetters.62b3efaa.png"
+  },
+  {
+    "revision": "254770b3820e19cb1ae1fcc7b6f5cb0a",
+    "url": "/static/media/SendViaFile.254770b3.gif"
+  },
+  {
+    "revision": "c95607f0e9c33ab8042d64aef15f69ff",
+    "url": "/static/media/telegram-512.c95607f0.png"
+  },
+  {
+    "revision": "b00c34307c27922ce8d02d97683d6d92",
+    "url": "/index.html"
+  }
+];
